8c8e1cdc0f814ff5ef29c2ee99f46fbe877babd6
Jiyong-ko <kjy90516@naver.comkjy90516@daum.net>
2025-04-14 15:37:11 +0900

    * 비동기 함수 테스트 케이스 작성

--- HelloWorld/HelloWorld/FizzBuzz.swift
+++ HelloWorld/HelloWorld/FizzBuzz.swift
@@ -5,6 +5,20 @@
 //  Created by NoelMacMini on 4/14/25.
 //
 
-public func fizzBuzz(_ number: Int) -> String {
-   return ""
- }
+func fizzBuzz(_ number: Int) -> String {
+  if number % 3 == 0 && number % 5 == 0 {
+    return "fizz-buzz"
+  } else if number % 3 == 0 {
+    return "fizz"
+  } else if number % 5 == 0 {
+    return "buzz"
+  } else {
+    return "\(number)"
+  }
+}
+
+func asyncSum(a: Int, b: Int, complete: @escaping (Int) -> Void) async {
+  // Simulate some asynchronous work
+  try? await Task.sleep(nanoseconds: 200_000_000)
+  complete(a + b)
+}
