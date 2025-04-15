b5e5653da20bc737e09814168d9c1122b7adfb83
Jiyong-ko <kjy90516@naver.comkjy90516@daum.net>
2025-04-15 15:20:23 +0900

    MenuItem 구조체에 spicy 속성 추가 및 테스트를 위한 fixture 메서드 구현, MenuGroupingTests에서 fixture 사용

---
+++ Albertos/AlbertosTests/MenuItem+Fixture.swift
@@ -0,0 +1,22 @@
+//
+//  MenuItem+Fixture.swift
+//  Albertos
+//
+//  Created by NoelMacMini on 4/15/25.
+//
+
+@testable import Albertos
+
+extension MenuItem {
+    static func fixture(
+        category: String = "category",
+        name: String = "name",
+        spicy: Bool = false
+    ) -> MenuItem {
+        MenuItem(
+            category: category,
+            name: name,
+            spicy: spicy
+        )
+    }
+}
