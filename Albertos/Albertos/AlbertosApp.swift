9886ab8bf062cc6781b72b01483125d3805bfdee
Jiyong-ko <kjy90516@naver.comkjy90516@daum.net>
2025-04-15 11:29:44 +0900

    챕터 4: 새 프로젝트 생성(Albertos)

---
+++ Albertos/Albertos/AlbertosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlbertosApp.swift
+//  Albertos
+//
+//  Created by NoelMacMini on 4/15/25.
+//
+
+import SwiftUI
+
+@main
+struct AlbertosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
