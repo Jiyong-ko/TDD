193bea113c39c2a8fce1e55a08ef807b342c26f5
Jiyong-ko <kjy90516@naver.comkjy90516@daum.net>
2025-04-15 13:16:46 +0900

    Models.swift 파일 추가 및 메뉴 카테고리별 그룹화 기능 구현

--- Albertos/AlbertosTests/MenuGroupingTests.swift
+++ Albertos/AlbertosTests/MenuGroupingTests.swift
@@ -11,16 +11,28 @@ import XCTest
 final class MenuGroupingTests: XCTestCase {
     // 한 카테고리당 섹션이 하나씩 있어야 한다.
     func testMenuWithManyCatetoriesReturnsOneSectionPerCategory() {
-        
+
     }
     
     // 카테고리가 하나인 경우 섹션도 하나여야 한다.
-    func testMenuWithOneCategoryReturnsOneSection() {}
+    func testMenuWithOneCategoryReturnsOneSection() {
+        // Arrange: 카테고리가 하나인 메뉴
+        let menu = [
+            MenuItem(category: "pastas"),
+            MenuItem(category: "pastas")
+        ]
+
+        // Act
+        let sections = groupMenuByCategory(menu)
+
+        // Assert
+        XCTAssertEqual(sections.count, 1)
+    }
     
     // 메뉴가 비어있으면 섹션도 비어있어야 한다.
     func testEmptyMenuReturnsEmptySections() {
         // Arrange
-        let sections: [MenuSection]()
+        let menu = [MenuItem]()
         
         // Act
         let sections = groupMenuByCategory(menu)
