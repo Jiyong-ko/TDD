d9802d0f0572ef41129edfaac5f933f44721075e
Jiyong-ko <kjy90516@naver.comkjy90516@daum.net>
2025-04-16 14:40:00 +0900

    MenuFetchingPlaceholder에서 메뉴 가져오기 로직을 랜덤 실패 및 성공으로 수정하고, MenuList 뷰에서 오류 발생 시 재시도 버튼 추가

--- Albertos/Albertos/MenuFetchingPlaceholder.swift
+++ Albertos/Albertos/MenuFetchingPlaceholder.swift
@@ -11,7 +11,15 @@ import Combine
 
 class MenuFetchingPlaceholder: MenuFetching {
   func fetchMenu() -> AnyPublisher<[MenuItem], Error> {
-    return Future { $0(.success(menu)) }
+    return Future {
+        // 랜덤하게 실패와 성공을 발행
+        let isSuccess = Bool.random()
+        if isSuccess {
+            $0(.success(menu))
+        } else {
+            $0(.failure(NSError(domain: "API 호출에 실패 했습니다.", code: -1, userInfo: nil)))
+        }
+    }
       .delay(for: 0.5, scheduler: RunLoop.main)
       .eraseToAnyPublisher()
   }
