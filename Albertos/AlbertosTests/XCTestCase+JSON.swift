3874f6989d6f9745e1a710dd87e5ee8b0177a4a4
Jiyong-ko <kjy90516@naver.comkjy90516@daum.net>
2025-04-17 11:19:10 +0900

    MenuItemTests에서 JSON 디코딩을 위한 jsonFixture 메서드 추가 및 XCTestCase에 JSON 파일 로드 기능 구현

---
+++ Albertos/AlbertosTests/XCTestCase+JSON.swift
@@ -0,0 +1,18 @@
+//
+//  XCTestCase+JSON.swift
+//  Albertos
+//
+//  Created by NoelMacMini on 4/17/25.
+//
+
+
+import XCTest
+
+extension XCTestCase {
+  func dataFromJSONFileNamed(_ name: String) throws -> Data {
+    let url = try XCTUnwrap(
+      Bundle(for: type(of: self)).url(forResource: name, withExtension: "json")
+    )
+    return try Data(contentsOf: url)
+  }
+}
