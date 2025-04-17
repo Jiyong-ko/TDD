094a5f21faa508dd519346ac796168e12b85abfa
Jiyong-ko <kjy90516@naver.comkjy90516@daum.net>
2025-04-17 11:33:43 +0900

    MenuItem 구조체의 카테고리 속성을 수정하고 JSON 디코딩 테스트를 업데이트함

--- Albertos/AlbertosTests/MenuItem+JSONFixture.swift
+++ Albertos/AlbertosTests/MenuItem+JSONFixture.swift
@@ -16,7 +16,10 @@ extension MenuItem {
     ) -> String {
         """
         {
-            "category": "\(category)",
+
+            "category": {
+              "name": "\(category)"
+            },
             "name": "\(name)",
             "spicy": \(spicy),
             "price": \(price)
