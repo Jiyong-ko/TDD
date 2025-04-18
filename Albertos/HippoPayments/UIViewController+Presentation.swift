2277addfdb27f4f44a0eb6bb31974f6ae26b9e3e
Jiyong-ko <kjy90516@naver.comkjy90516@daum.net>
2025-04-18 14:28:21 +0900

    HippoPayments 프레임워크 추가 및 관련 클래스 구현

---
+++ Albertos/HippoPayments/UIViewController+Presentation.swift
@@ -0,0 +1,18 @@
+//
+//  UIViewController+Presentation.swift
+//  Albertos
+//
+//  Created by NoelMacMini on 4/18/25.
+//
+
+import UIKit
+ 
+ extension UIViewController {
+ 
+   /// Travels the `presentedViewController` hierarchy backwards till it finds the topmost one.
+   var viewControllerPresentationSource: UIViewController {
+     guard let presentedViewController = self.presentedViewController else { return self }
+ 
+     return presentedViewController.viewControllerPresentationSource
+   }
+ }
